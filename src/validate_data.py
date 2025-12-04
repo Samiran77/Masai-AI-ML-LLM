8c35c853fabb0be2fa9058bf168c9852c3fa67f7
Samiran Mahato <samiranmahato77@gmail.com>
2025-12-04 19:42:27 +0530

    Added data preparation and validation

---
+++ src/validate_data.py
@@ -0,0 +1,38 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+TRAIN_PATH = Path('./data/processed/train.csv')
+
+EXPECTED_COLUMNS = [
+    "sepal length (cm)",
+    "sepal width (cm)",
+    "petal length (cm)",
+    "petal width (cm)",
+    "target"
+]
+
+def main() -> int:
+    if not TRAIN_PATH.exists():
+        print(f"Error: {TRAIN_PATH} does not exist.", file=sys.stderr)
+        sys.exit(1)
+    
+    df = pd.read_csv(TRAIN_PATH)
+    
+    #Column check
+    cols = list[Any](df.columns)
+    if(cols != EXPECTED_COLUMNS):
+        print(f"Error: {TRAIN_PATH} does not contains the expected columns.")
+        return 1;
+
+    #NULL check
+    null_counts = df.isnull().sum()
+    if(null_counts.any()):
+        print(f"Error: {TRAIN_PATH} contains null values:\n")
+        return 1;
+    
+    print(f"[OK] Data validation passed for {TRAIN_PATH}")
+    
+    return 0;
+    
